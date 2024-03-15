39b5ec50a0ef5364f0f215d9dd905e1c6c3bad50
kajal-dizayn <124876106+kajal-dizayn@users.noreply.github.com>
2024-03-15 14:05:15 +0530

    ui practice

---
+++ y/app/about/page.tsx
@@ -0,0 +1,21 @@
+import { TextGenerateEffectDemo } from "@/component/GeneratorEffect";
+import { TextRevealCardPreview } from "@/component/TextEffect";
+import { TracingBeamDemo } from "@/component/TracingBeam";
+import { TailwindcssButtons } from "@/component/buttons";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      {/* <TailwindcssButtons />
+
+      <TextGenerateEffectDemo /> */}
+
+      {/* <TextRevealCardPreview /> */}
+
+      <TracingBeamDemo />
+    </div>
+  );
+};
+
+export default page;
